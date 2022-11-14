11be1fde28b685f5e6904eda8a9a1d7c91474b2d
vmpr0tect <68221218+vmpr0tect@users.noreply.github.com>
2022-11-14 23:58:16 +0100

    first commit

---
+++ Skye/Program.cs
@@ -0,0 +1,14 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Skye;
+
+DiscordClient client = new DiscordClient(new DiscordConfiguration()
+{
+    Token = "MTA0MTg0NjU2NzU1ODIwNTU0MA.GNr9RZ.RDti_VARBHU5z1LE4mamoiaLAnIy9D5d40rF60",
+    TokenType = TokenType.Bot
+});
+
+client.UseSlashCommands().RegisterCommands<SlashCommands>();
+
+await client.ConnectAsync();
+Thread.Sleep(-1);
---
+++ Skye/SlashCommands.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace Skye;
+
+public class SlashCommands : ApplicationCommandModule
+{
+    [SlashCommand("ping", "go kys iskra")]
+    public async Task PingCommand(InteractionContext context) => await context.CreateResponseAsync("du bist schwul");
+}
